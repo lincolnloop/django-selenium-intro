37ebda16cf292d8b8c187c29f85a4baec9fe77bb
Brian Luft <brian@unbracketed.com>
2012-11-04 06:38:03 -0800

    Adds missing setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="django-selenium-intro",
+    version='1.0',
+    description="",
+    author="Lincoln Loop",
+    author_email='info@lincolnloop.com',
+    url='',
+    packages=find_packages(),
+    scripts=['manage.py'],
+)
